c82703cfb0a75985fd73cd2f263b096796639bc0
Chloe <page.prologue@gmail.com>
2020-09-03 23:08:23 +0900

    mdn canvas tutorial - Drawing rectangles

--- js/canvas/Canvas.js
+++ js/canvas/Canvas.js
@@ -1,4 +1,4 @@
-function draw() {
+function draw1() {
     var canvas = document.getElementById('tutorial');
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
@@ -8,5 +8,25 @@ function draw() {
 
         ctx.fillStyle = 'rgba(0, 0, 200, 0.5)'; // blue
         ctx.fillRect(30, 30, 50, 50);
+    }
+}
+
+
+/** Drawing rectangles
+ * - fillRect(x, y, width, height)
+ *   채워진 사각형을 그립니다.
+ * - strokeRect(x, y, width, height)
+ *   직사각형 윤곽을 그립니다.
+ * - clearRect(x, y, width, height)
+ *   지정된 사각형 영역을 지우고 완전히 투명하게 만듭니다.
+**/
+function draw2() {
+    var canvas = document.getElementById('tutorial');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        ctx.fillRect(25, 25, 100, 100);
+        ctx.clearRect(45, 45, 60, 60);
+        ctx.strokeRect(50, 50, 50, 50);
     }
 }
