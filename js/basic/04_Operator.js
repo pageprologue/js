a04fd722ef9b4f0c8bf924a4344c167b68b92c58
Chloe <page.prologue@gmail.com>
2020-09-28 18:30:56 +0900

    Operator

--- js/basic/04_Operator.js
+++ js/basic/04_Operator.js
@@ -178,3 +178,142 @@ var x, y;
 y = x = 10; // 연쇄 할당(Chained assignment)
 console.log(x, y); // 10 10
 
+
+/**
+ * 4. 비교 연산자(Comparison Operator): 좌항과 우항의 피연산자를 비교하여 불리언 값을 반환한다. 
+ */
+ // 동등 비교
+5 == 5    // true
+// 타입은 다르지만 암묵적 타입 변환을 통해 타입을 일치시키면 같은 값을 같는다.
+5 == '5'   //true
+5 == 8    // false
+
+'' == '0'           // false
+0 == ''             // true
+0 == '0'            // true
+
+false == 'false'    // false
+false == '0'        // true
+
+false == undefined  // false
+false == null       // false
+null == undefined   // true
+
+// 일치 비교
+5 === 5   // true
+5 === '5' // false
+NaN === NaN // false
+isNaN(NaN) // true
+0 === -0     // true
+
+// 부동등 비교
+5 != 8    // true
+5 != 5    // false
+5 != '5'  // false
+
+// 불일치 비교
+5 !== 8   // true
+5 !== 5   // false
+5 !== '5' // true
+
+// 대소 관계 비교
+5 > 0    // true
+5 > 5    // false
+5 > 8    // false
+5 < 0    // false
+5 < 5    // false
+5 < 8    // true
+5 >= 0   // true
+5 >= 5   // true
+5 >= 8   // false
+5 <= 0   // false
+5 <= 5   // true
+5 <= 8   // true
+
+
+/**
+ * 5. 삼항 조건 연산자(ternary operator): 조건식의 평가 결과에 따라 반환할 값을 결정한다. 
+ * - 조건식 ? 조건식이 ture일때 반환할 값 : 조건식이 false일때 반환할 값
+ */
+var x = 2;
+
+// x가 짝수이면 '짝수'를 홀수이면 '홀수'를 반환한다.
+// 2 % 2는 0이고 0은 false로 암묵적 타입 변환된다.
+var result = x % 2 ? '홀수' : '짝수';
+console.log(result); // 짝수
+
+var x = 2, result;
+
+// x가 짝수이면 '짝수'를 홀수이면 '홀수'를 반환한다.
+// 2 % 2는 0이고 0은 false로 암묵적 타입 변환된다.
+if (x % 2) result = '홀수';
+else       result = '짝수';
+console.log(result); // 짝수
+
+
+/**
+ * 6. 논리 연산자(Logical Operator): 우항과 좌항의 피연산자(부정 논리 연산자의 경우, 우항의 피연산자)를 논리 연산한다.
+ */
+// 논리합(||) 연산자
+true || true   // true
+true || false  // true
+false || true  // true
+false || false // false
+
+// 논리곱(&&) 연산자
+true && true   // true
+true && false  // false
+false && true  // false
+false && false // false
+
+// 논리 부정(!) 연산자
+!true  // false
+!false // true
+
+// 암묵적 타입 변환
+!0 // true
+
+// 단축 평가
+'Cat' && 'Dog' // “Dog”
+
+
+/**
+ * 7. 쉼표 연산자
+ */
+var x, y, z;
+x = 1, y = 2, z = 3; // 3
+
+
+/**
+ * 8. 그룹 연산자
+ */
+10 * 2 + 3   // 23
+10 * (2 + 3) // 50
+
+
+/**
+ * 9. typeof 연산자
+ */
+typeof ''              // "string"
+typeof 1               // "number"
+typeof NaN             // "number"
+typeof true            // "boolean"
+typeof undefined       // "undefined"
+typeof Symbol()        // "symbol"
+typeof null            // "object"
+typeof []              // "object"
+typeof {}              // "object"
+typeof new Date()      // "object"
+typeof /test/gi        // "object"
+typeof function () {}  // "function"
+
+// 주의해야 할 것은 typeof 연산자로 null 값을 연산해 보면 null이 아닌 “object”를 반환한다는 것이다.
+// null 타입을 확인할 때는 typeof 연산자를 사용하지 말고 일치 연산자(===)를 사용하도록한다.
+typeof null  // "object"
+var foo = null;
+console.log(typeof foo === null); // false
+console.log(foo === null);        // true
+
+// 선언하지 않은 식별자를 typeof 연산자로 연산해 보면 ReferenceError가 발생하지 않고 “undefined”를 반환한다.
+typeof undeclared  // "undefined"
+
